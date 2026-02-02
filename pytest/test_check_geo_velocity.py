dabddcfcf872ef097dbf4c805e9e96de0fb5ce52
Martin Gabriel Gardea <gardemar@fit.cvut.cz>
2026-02-02 18:49:27 +0100

    fix: Change test to work with modified function names

--- pytest/test_check_geo_velocity.py
+++ pytest/test_check_geo_velocity.py
@@ -1,12 +1,12 @@
 import pytest, json
-from main import getGeoVelocity
+from main import check_geo_velocity
 
-def test_getGeoVelocity():
-    assert getGeoVelocity(
+def test_check_geo_velocity():
+    assert check_geo_velocity(
         json.loads('{"tx_id": "T1", "account_id": "A1", "timestamp": "2023-01-01T10:00:00", "amount": 100.0, "location": {"lat": 40.7128, "lon": -74.0060}, "device_id": "D1"}'), 
         json.loads('{"tx_id": "T2", "account_id": "A1", "timestamp": "2023-01-01T10:05:00", "amount": 50.0, "location": {"lat": 48.8566, "lon": 2.3522}, "device_id": "D1"}')
         ) == True
-    assert getGeoVelocity(
+    assert check_geo_velocity(
         json.loads('{"tx_id": "T1", "account_id": "A1", "timestamp": "2023-01-01T10:00:00", "amount": 100.0, "location": {"lat": 40.7128, "lon": -74.0060}, "device_id": "D1"}'), 
         json.loads('{"tx_id": "T2", "account_id": "A1", "timestamp": "2023-01-02T10:05:00", "amount": 50.0, "location": {"lat": 48.8566, "lon": 2.3522}, "device_id": "D1"}')
         ) == False
--- pytest/test_get_delta_time.py
+++ pytest/test_get_delta_time.py
@@ -1,16 +1,16 @@
 import pytest, json
-from main import getDeltaTime
+from main import get_delta_time
 
-def test_getDeltaTime():
-    assert getDeltaTime(
+def test_get_delta_time():
+    assert get_delta_time(
         json.loads('{"tx_id": "T1", "account_id": "A1", "timestamp": "2023-01-01T10:00:00", "amount": 100.0, "location": {"lat": 40.7128, "lon": -74.0060}, "device_id": "D1"}'), 
         json.loads('{"tx_id": "T2", "account_id": "A1", "timestamp": "2023-01-01T10:05:00", "amount": 50.0, "location": {"lat": 48.8566, "lon": 2.3522}, "device_id": "D1"}')
         ) == 1/12
-    assert getDeltaTime(
+    assert get_delta_time(
         json.loads('{"tx_id": "T1", "account_id": "A1", "timestamp": "2023-01-01T10:00:00", "amount": 100.0, "location": {"lat": 40.7128, "lon": -74.0060}, "device_id": "D1"}'), 
         json.loads('{"tx_id": "T2", "account_id": "A1", "timestamp": "2023-01-01T10:00:00", "amount": 50.0, "location": {"lat": 48.8566, "lon": 2.3522}, "device_id": "D1"}')
         ) == 0
-    assert getDeltaTime(
+    assert get_delta_time(
         json.loads('{"tx_id": "T1", "account_id": "A1", "timestamp": "2023-01-01T10:00:00", "amount": 100.0, "location": {"lat": 40.7128, "lon": -74.0060}, "device_id": "D1"}'), 
         json.loads('{"tx_id": "T2", "account_id": "A1", "timestamp": "2023-01-02T10:00:00", "amount": 50.0, "location": {"lat": 48.8566, "lon": 2.3522}, "device_id": "D1"}')
         ) == 24
